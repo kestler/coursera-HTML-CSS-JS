793dd6f5751bc0f0e22076d264ce45d52cbe0cd8
Kevin Kestler <kevin.kestler@gmail.com>
2016-07-17 01:32:15 -0400

    Corrected window issue

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -1,6 +1,6 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
-(function() {
+(function(window) {
 	var speakWord = "Hello";
 
 	// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
@@ -22,4 +22,4 @@
 	// See Lecture 52, part 2
 	// (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 	window.helloSpeaker = helloSpeaker;
-})();
+})(window);
